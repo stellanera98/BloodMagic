357b9ef61a8de5cb35d1e624287430e7b5e08ae6
stellanera98 <luanastellas98@gmail.com>
2025-02-28 12:10:25 +0100

    plz just let me

--- src/datagen/java/wayoftime/bloodmagic/Datagen.java
+++ src/datagen/java/wayoftime/bloodmagic/Datagen.java
@@ -5,9 +5,12 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+import wayoftime.bloodmagic.block.BMBlocks;
 import wayoftime.bloodmagic.datagen.providers.*;
+import wayoftime.bloodmagic.tag.BMTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -32,5 +35,15 @@ public class Datagen {
         BMDatapackProvider packProvider = new BMDatapackProvider(output, registries);
         generator.addProvider(event.includeServer(), packProvider);
         generator.addProvider(event.includeServer(), new BMAltarTierTagProvider(output, packProvider.getRegistryProvider(), existingFileHelper));
+
+        PackOutput packsBase = new PackOutput(output.getOutputFolder().resolve("packs"));
+        PackOutput t6output = new PackOutput(packsBase.getOutputFolder().resolve("tier6"));
+
+        generator.addProvider(event.includeServer(), new BlockTagsProvider(t6output, registries, BloodMagic.MODID, existingFileHelper) {
+            @Override
+            protected void addTags(HolderLookup.Provider provider) {
+                tag(BMTags.Blocks.T6_CAP);
+            }
+        });
     }
 }
