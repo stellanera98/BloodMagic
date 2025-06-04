ab910f1a51513bbb01aa244248f124be210f2a92
StellaNera98 <luanastellas98@gmail.com>
2025-06-04 05:44:08 +0200

    i didnt even know there was this thing, but living elytra layer here we go

--- src/main/java/wayoftime/bloodmagic/client/event/ClientModEventHandler.java
+++ src/main/java/wayoftime/bloodmagic/client/event/ClientModEventHandler.java
@@ -1,10 +1,14 @@
 package wayoftime.bloodmagic.client.event;
 
+import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.client.resources.PlayerSkin;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import wayoftime.bloodmagic.BloodMagic;
+import wayoftime.bloodmagic.client.render.entity.layer.LivingElytraLayer;
 import wayoftime.bloodmagic.common.datacomponent.BMDataComponents;
 import wayoftime.bloodmagic.common.datacomponent.EnumWillType;
 import wayoftime.bloodmagic.common.item.BMItems;
@@ -20,5 +24,15 @@ public class ClientModEventHandler {
             });
             ItemProperties.register(BMItems.SACRIFICIAL_DAGGER.get(), BloodMagic.INCENSE_PROPERTY, ((stack, level, entity, seed) -> stack.getOrDefault(BMDataComponents.INCENSE, false) ? 1 : 0));
         });
+    }
+
+    @SubscribeEvent
+    public static void registerRenderLayer(EntityRenderersEvent.AddLayers event) {
+        // TODO figure out if there is a better way to get a whatever renderer that has #addLayer on it
+        PlayerRenderer skin = event.getSkin(PlayerSkin.Model.WIDE);
+        skin.addLayer(new LivingElytraLayer<>(skin, event.getEntityModels()));
+
+        skin = event.getSkin(PlayerSkin.Model.SLIM);
+        skin.addLayer(new LivingElytraLayer<>(skin, event.getEntityModels()));
     }
 }
